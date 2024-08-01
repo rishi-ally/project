78600d772a99bb0136ccc3903d616802d0633016
rishi-ally <rishikeshpurohit94@gmail.com>
2024-08-01 13:25:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
 
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
 function navAnimation() {
     var nav = document.querySelector("nav")
     
@@ -138,6 +172,23 @@ function navAnimation() {
     
     }
     
+function page6Animations() {
+    gsap.from("#btm8-part2 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm8-part2",
+            scroller: "#main",
+            // markers:true,
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+}
+
+locomotiveAnimation()
+
     
     section ()
     page2animation()
